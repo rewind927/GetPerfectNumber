6999c27ce87f21d91154553441926ca8787fee07
rewind927 <rewind927@gmail.com>
2014-09-11 12:00:10 +0800

    modify the code to improve the performance

--- PerfectNumber.py
+++ PerfectNumber.py
@@ -1,18 +1,12 @@
-
+import sys
 
 def isPerfectNumber(number):
     sum = 0
-    for element in range(1,number):
+    for element in range(1, number/2+1):
         if number % element == 0:
             sum += element
     return sum == number
 
-
-while True:
-    number = raw_input("Input number: ")
-    if number.isdigit(): 
-        print("%s is perfect number ? %s" %(number,isPerfectNumber(int(number))))
-        print("-------------------------------------")
-    else:
-        print("not digital number, break this loop!")
-        break 
+number = sys.argv[1]
+print("%s is perfect number ? %s" %(number,isPerfectNumber(int(number))))
+print("-------------------------------------")
